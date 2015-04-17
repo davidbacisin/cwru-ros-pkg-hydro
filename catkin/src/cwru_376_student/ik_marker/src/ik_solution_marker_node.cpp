22a3c02e71a97c03e7da08d95dcbbebd77b249cd
SU LU <sulu@ubuntu.(none)>
2015-04-17 12:19:20 -0400

    updated

--- catkin/src/cwru_376_student/ik_marker/src/ik_solution_marker_node.cpp
+++ catkin/src/cwru_376_student/ik_marker/src/ik_solution_marker_node.cpp
@@ -2,20 +2,49 @@
 // Wyatt Newman, demo how to place markers in rviz
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
+#include <irb120_kinematics.h>
 #include <geometry_msgs/Point.h>
+#include <std_msgs/Int16.h>
 
-int main(int argc, char** argv) {
-    ros::init(argc, argv, "simple_marker_placer"); // this will be the node name;
+
+// declaring some global variable
+geometry_msgs::Point makerPt;
+int bestIkSoluNum;
+
+
+// callback function
+void reachPtCallback(const geometry_msgs::Point& ptRcvd) {
+    // populate the message recieved from subscribed topic
+    makerPt.x = ptRcvd.x;
+    makerPt.y = ptRcvd.y;
+    makerPt.z = ptRcvd.z;
+    // debugging output
+    ROS_INFO("marker position: x = %f, y = %f, z  = %f", makerPt.x, makerPt.y, makerPt.z);
+}
+
+// callback function
+void bestIkSoluNumCallback(const std_msgs::Int16& bestIkSoluNumRcvd) {
+    // debugging output
+    ROS_INFO("The best IK solution No.: %i", bestIkSoluNumRcvd.data);
+    bestIkSoluNum = bestIkSoluNumRcvd.data;
+}
+
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "ik_solu_maker_placer");
     ros::NodeHandle nh;
 
-    ROS_INFO("hello, world");
-    ros::Rate timer(4); //timer to run at 2 Hz
+
+    //ROS_INFO("hello, world");
+    ros::Rate timer(1); //timer to run at 4 Hz
+    ros::Subscriber rechPtSub = nh.subscribe("reachablePt",1,reachPtCallback);
+    ros::Subscriber btIkNumSub = nh.subscribe("bestIkSoluNum",1,bestIkSoluNumCallback);
+
     // in rviz, "add" a "marker" and select this topic name: wsn_marker
-    ros::Publisher vis_pub = nh.advertise<visualization_msgs::Marker>( "sphere_list_marker", 0 );            
+    ros::Publisher vis_pub = nh.advertise<visualization_msgs::Marker>( "sphere_list_marker", 1 );            
     visualization_msgs::Marker marker;  // instantiate a marker object
     geometry_msgs::Point point;  // points will be used to specify where the markers go
-    //marker.header.frame_id = "/pelvis"; //base_link"; // select the reference frame 
-    marker.header.frame_id = "/base_link"; //base_link"; // select the reference frame
+    marker.header.frame_id = "/base_link"; //base_link"; // select the reference frame 
     marker.header.stamp = ros::Time();
     marker.ns = "my_namespace";
     marker.id = 0;
@@ -45,35 +74,4 @@ int main(int argc, char** argv) {
     marker.color.b = 0.0;
 
 
-    //as an example, let's build a wall of red spheres:
-    double dist = 1.0; //0.5; // robot distance from the wall, along x-axis in pelvis frame
-    double corner_y = -0.5; // y-coordinate of right edge of the wall
-    double corner_z = 0.0; // z-coordinate of lower-right corner of wall, as seen by Atlas
-    point.x = dist;
-    point.y = corner_y;
-    point.z = corner_z;
-        
-    for (int j = 0; j < 10; j++) {
-
-        point.y = corner_y;
-        point.x = dist;
-        marker.points.push_back(point);
-        vis_pub.publish( marker );
-        timer.sleep();
-        for (int k = 0; k < 10; k++) {
-            point.y += 0.1;
-            marker.points.push_back(point);
-            ROS_INFO("z,y = %f, %f",point.z,point.y);
-            vis_pub.publish( marker );
-             timer.sleep();
-        }
-        point.z += 0.1; // next row up...
-    }
-    
-   
-    for (int k=0; k<10; k++) {
-        vis_pub.publish( marker );     
-        timer.sleep();
-        ROS_INFO("publishing...");
-    }
 }
