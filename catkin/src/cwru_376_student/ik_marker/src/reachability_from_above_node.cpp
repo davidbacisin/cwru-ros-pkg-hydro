a3fa110db5c2e0971cd90264f8a2ffc0a04a3f52
SU LU <sulu@ubuntu.(none)>
2015-04-19 15:40:50 -0400

    updated

--- catkin/src/cwru_376_student/ik_marker/src/reachability_from_above_node.cpp
+++ catkin/src/cwru_376_student/ik_marker/src/reachability_from_above_node.cpp
@@ -9,17 +9,18 @@
 #include <irb120_kinematics.h>
 #include <geometry_msgs/Point.h>
 #include <std_msgs/Int16.h>
+#include <fstream>
 //#include <std_msgs/Int16MultiArray>
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "irb120_reachability");
-    ros::NodeHandle nh;
+    // ros::NodeHandle nh;
 
-    ros::Publisher reachPtPub = nh.advertise<geometry_msgs::Point>( "reachablePt", 1 ); // add a publisher
-    ros::Publisher bestIkSoluNumPub = nh.advertise<std_msgs::Int16>( "bestIkSoluNum", 1 ); // add a publisher
+    // ros::Publisher reachPtPub = nh.advertise<geometry_msgs::Point>( "reachablePt", 1 ); // add a publisher
+    // ros::Publisher bestIkSoluNumPub = nh.advertise<std_msgs::Int16>( "bestIkSoluNum", 1 ); // add a publisher
     // ros::Publisher reachPtPub = nh.advertise<std_msgs/Int16MultiArray>( "reachablePt",1 ); // add a publisher
     // ros::Publisher ikSoluNumPub = nh.advertise<std_msgs/Int16MultiArray>( "ikSoluNum", 1 ); // add a publisher
-    ros::Rate timer(1); // sleep timer for 2Hz repetition rate
+    // ros::Rate timer(1); // sleep timer for 5Hz repetition rate
 
     // create a variable of type "Point" to publish the desired point for manipulator's tool flange to move
     geometry_msgs::Point desPt;
@@ -62,6 +63,12 @@ int main(int argc, char **argv) {
     
     int nsolns = ik_solver.ik_solve(A_fwd_DH);*/
     std::vector<Vectorq6x1> q6dof_solns;
+    // std::vector<Eigen::Vector3d> reachPtVec;
+    //std::vector<Vector3d> reachPt_X_Vec;
+    //std::vector<Vector3d> reachPt_Y_Vec;
+    //std::vector<Vector3d> reachPt_Z_Vec;
+
+
     /*// Vectorq6x1 qsoln;
     ik_solver.get_solns(q6dof_solns);
     std::cout<<"IK solns: "<<std::endl;
@@ -74,61 +81,77 @@ int main(int argc, char **argv) {
     a_tool_des.linear() = R_des;
     // a_tool_des.translation() << x_des,0,0;
     // double x_des;
-    while (ros::ok()) {
-        std::cout<<std::endl<<"enter desPt_x: ";
-        std::cin>>desPt.x;
+    // while (true) {
+        // std::cout<<std::endl<<"enter desPt_x: ";
+        // std::cin>>desPt.x;
 
-        p[0] = desPt.x;
+        // p[0] = desPt.x;
+        // reachPtVec.clear();
         std::cout << "====  irb120 kinematics solver ====" << std::endl;
-        //int ans = 1;
-        //bool reachable_proposition;
-        for (double z_des = 0.9; z_des>-0.4; z_des-=0.1) {
-            std::cout<<std::endl;
-            std::cout<<"z="<< round(z_des*10)<<"  ";
-           for (double y_des=-1.0; y_des<1.0; y_des+= 0.05) {
-                p[1]= y_des;
-                p[2] = z_des;
-                /*desPt.y = y_des;
-                desPt.z = z_des;*/
-                a_tool_des.translation() = p;
-                int nsolns = ik_solver.ik_solve(a_tool_des);
-                //std_msgs::Int16 iknsolns; // create a variable of type "Int16" to publish the number of IK solution
-                //iknsolns.data = nsolns;
-                std::cout<<nsolns;
 
-                // Only when nsolns > 0, it is meaningful to publish the corresponding desired tool point as a topic: reachablePt
-                if (nsolns>0) {
-                    desPt.y = y_des; // remember the desired y and and assign to destPt.y
-                    desPt.z = z_des; // remember the desired z and and assign to destPt.z
-                    ik_solver.get_solns(q6dof_solns);
-                    // defining a joint limits vector for joint 0 and joint 1, such that each joint is specified within a range of motion
-                    std::vector<double> jointLimits {0,-M_PI,-M_PI/2,M_PI/6}; 
-                    std::vector<int> weight{1,2,3,3,2,1}; //defining a weight vector
-                    double sum;
-                    double minimum = 1e6;
-                    std_msgs::Int16 bestIkSoluNo;
-                    Vectorq6x1 oneIkSolu;
-                    // Once nsolns > 0, choose a IK solution both meet the specified joint limit the weight requirement
-                    for (int i = 0; i < q6dof_solns.size(); ++i){
-                        oneIkSolu = q6dof_solns[i];
-                        if (oneIkSolu[0] < jointLimits[0] && oneIkSolu[0] > jointLimits[1] 
-                            && oneIkSolu[1] < jointLimits[3] && oneIkSolu[1] > jointLimits[2]) {
-                            sum = 0;
-                            for (int ijnt = 0; ijnt < 6; ++ijnt){
-                                sum = sum + oneIkSolu[ijnt] * weight[ijnt];
-                            }
-                            if (sum < minimum){
-                                minimum = sum;
-                                bestIkSoluNo.data = i; // remember the IK solution which has the minimum last joint angle solution
-                            }
+        // output a File 
+        std::ofstream outputFile;
+        // std::ios::trunc If the file is opened for output operations and it already existed, its previous content is deleted and replaced by the new one.
+        outputFile.open("/home/sulu/ros_workspace/cwru-ros-pkg-hydro-beta/catkin/src/cwru_376_student/ik_marker/mktPtPos.txt");
+        for (double x_des = -0.4; x_des < 0.7; x_des += 0.1) {
+            std::cout << std::endl;
+            std::cout << "x=" << x_des <<"  ";
+            for (double z_des = 0.9; z_des >- 0.4; z_des -= 0.1) {
+                std::cout << std::endl;
+                std::cout << "z=" << round(z_des*10) << "  ";
+               for (double y_des =- 1.0; y_des < 1.0; y_des += 0.05) {
+                    p[0] = x_des;
+                    p[1]= y_des;
+                    p[2] = z_des;
+                    /*desPt.y = y_des;
+                    desPt.z = z_des;*/
+                    a_tool_des.translation() = p;
+                    int nsolns = ik_solver.ik_solve(a_tool_des);
+                    //std_msgs::Int16 iknsolns; // create a variable of type "Int16" to publish the number of IK solution
+                    //iknsolns.data = nsolns;
+                    std::cout<<nsolns;
+
+                    // Only when nsolns > 0, it is meaningful to publish the corresponding desired tool point as a topic: reachablePt
+                    if (nsolns>0) {
+                        desPt.x = x_des; // remember the desired value of x coordiate and and assign to desPt.x
+                        desPt.y = y_des; // remember the desired value of y coordiate and and assign to desPt.y
+                        desPt.z = z_des; // remember the desired value of z coordiate and and assign to desPt.z
+                        //ROS_INFO("desired point: x = %f, y = %f, z = %f", desPt.x, desPt.y, desPt.z);
+                        ik_solver.get_solns(q6dof_solns);
+                        // defining a joint limits vector for joint 0 and joint 1, such that each joint is specified within a range of motion
+                        std::vector<double> jointLimits {0,-M_PI,-M_PI/2,M_PI/6}; 
+                        std::vector<int> weight{1,2,3,3,2,1}; //defining a weight vector
+                        double sum;
+                        double minimum = 1e6;
+                        std_msgs::Int16 bestIkSoluNo;
+                        Vectorq6x1 oneIkSolu;
+                        // Once nsolns > 0, choose a IK solution both meet the specified joint limit the weight requirement
+                        for (int i = 0; i < q6dof_solns.size(); ++i){
+                            oneIkSolu = q6dof_solns[i];
+                            if (oneIkSolu[0] < jointLimits[0] && oneIkSolu[0] > jointLimits[1] 
+                                && oneIkSolu[1] < jointLimits[3] && oneIkSolu[1] > jointLimits[2]) {
+                                sum = 0;
+                                for (int ijnt = 0; ijnt < 6; ++ijnt){
+                                    sum = sum + oneIkSolu[ijnt] * weight[ijnt];
+                                }
+                                if (sum < minimum){
+                                    minimum = sum;
+                                    bestIkSoluNo.data = i; // remember the IK solution which has the minimum last joint angle solution
+                                }
+                            }   
                         }
-                        bestIkSoluNumPub.publish(bestIkSoluNo); // publish the value--of type stf_msgs::Int16-- to the topic "ikSoluNum"
-                        reachPtPub.publish(desPt); // publish the value--of type geometry_msgs::Point-- to the topic "reachablePt"
+                        // bestIkSoluNumPub.publish(bestIkSoluNo); // publish the value--of type stf_msgs::Int16-- to the topic "ikSoluNum"
+                        // reachPtPub.publish(desPt); // publish the value--of type geometry_msgs::Point-- to the topic "reachablePt"
+                        outputFile << desPt << std::endl;
                     }
                 }
             }
         }
-        timer.sleep();
-    }
+        // for (int i = 0; i < reachPtVec.size(); ++i) {
+        //     ROS_INFO("Reachable Point Coordinates: X = %f, Y= %f, Z = %f", reachPtVec[&i].x ,reachPtVec[&i].y, reachPtVec[&i].z);
+        // }
+        outputFile.close();
+        // timer.sleep();
+    // }
     return 0;
 }
